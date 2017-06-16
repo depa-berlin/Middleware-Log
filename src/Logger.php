36c669318b18d7112a1b85916f0c4dddf2267b4c
Mirko Fenrich <fenrich@designpark.de>
2017-06-16 23:59:44 +0200

    Versuch Objekte zu loggen

--- src/Logger.php
+++ src/Logger.php
@@ -3,23 +3,78 @@ namespace Depa\MiddlewareLogger;
 
 use Psr\Log\LoggerInterface;
 
-class Logger{
-    
-    static public $logger;    
-    
+class Logger
+{
+
+    public static $staticLogger;
+    public $logger;
+
     public function __construct(LoggerInterface $logger)
     {
-        self::$logger = $logger; 
+        self::$staticLogger = $logger;
+        $this->logger = $logger;
     }
-    
 
-    
-    public function __call($name, $arguments){
-        $this->logger->$name($arguments);
+    public function __call($name, $arguments)
+    {
+        $argumentsArray = implode(', ', $arguments);
+        if($name == 'log'){
+            $argumentsArray[1] = self::prepearObject($argumentsArray[1]);
+        }
+        else{
+            $argumentsArray[0] = self::prepearObject($argumentsArray[0]);
+        }
+        switch (count($argumentsArray)) {
+            case 1:
+                $this->logger->$name($argumentsArray[0]);
+                break;
+            case 1:
+                $this->logger->$name($argumentsArray[0], $argumentsArray[1]);
+                break;
+            case 1:
+                $this->logger->$name($argumentsArray[0], $argumentsArray[1], $argumentsArray[2]);
+                break;
+        }
     }
-    
+
+
     public static function __callStatic($name, $arguments)
     {
-        self::$logger->$name($arguments);
+        $argumentsArray = implode(', ', $arguments);
+        if($name == 'log'){
+            $argumentsArray[1] = self::prepearObject($argumentsArray[1]);
+        }
+        else{
+            $argumentsArray[0] = self::prepearObject($argumentsArray[0]);
+        }
+        switch (count($argumentsArray)) {
+            case 1:
+                self::$staticLogger->$name($argumentsArray[0]);
+                break;
+            case 1:
+                self::$staticLogger->$name($argumentsArray[0], $argumentsArray[1]);
+                break;
+            case 1:
+                self::$staticLogger->$name($argumentsArray[0], $argumentsArray[1], $argumentsArray[2]);
+                break;
+        }
     }
+    
+    private static function prepearObject($obj){
+        if(!is_object($obj))
+        {
+            return $obj;
+        }
+        $ret = [];
+        $ret[get_class($obj)] = [];
+        foreach (get_object_vars($obj) as $name => $content) {
+            if (!is_object($content)) {
+                $ret[$name] = ['type' => gettype($content), 'content' => $content];
+            } else {
+                $ret[$name] = ['type' => gettype($content), 'class' => get_class($content)];
+            }
+        }
+        return $ret;
+    }
+    
 }
